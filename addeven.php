1e204aae0c1ad0b1f1f9d64d017d74c91d37e446
Jhayr Million <benjaminmillion000@gmail.com>
2025-03-15 16:09:34 +0800

    activity

---
+++ addeven.php
@@ -0,0 +1,16 @@
+<?php 
+function oddeven($number){
+    if (!is_numeric($number)) {
+        return "Invalid input: Please provide a number.";
+    }
+    if ($number % 2 == 0) {
+        return "even";
+} else {    
+    return "odd";
+} }
+
+$number = 3; 
+$result = oddeven($number);
+echo "The number " . $number . " is " . $result . ".";
+
+?>
