03070f06f9bb94460e533c015da44065e6b31ac9
Jhayr Million <benjaminmillion000@gmail.com>
2025-03-15 16:14:49 +0800

    new branch

---
+++ function.php
@@ -0,0 +1,12 @@
+function oddeven($number){
+    if (!is_numeric($number)) {
+        return "Invalid input: Please provide a number.";
+    }
+    if ($number % 2 == 0) {
+        return "even";
+} else {    
+    return "odd";
+} }
+
+$number = 3; 
+$result = oddeven($number);
